e4778be985236d84b156a0acbbc87253a5b923b5
thanapol.kha <thanapol.kha@tqmtrainee.co.th>
2026-09-12 17:17:11 +0700

    [dev]creat endpoint todoapi by id

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -23,6 +23,13 @@ var todos = new List<TodoGetDto>
 
 app.MapGet("/api/todos",()=> Results.Ok(todos));
 
+app.MapGet("/api/todos/{id}",(int id)=>
+{
+    var todo = todos.FirstOrDefault(t=> t.Id == id);
+
+    return todo is null ? Results.NotFound() : Results.Ok(todo);
+});
+
 
 app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
