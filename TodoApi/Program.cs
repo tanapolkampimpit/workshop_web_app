0e59f280e61058333bdf9cfba2072a5a40d10dbf
thanapol.kha <thanapol.kha@tqmtrainee.co.th>
2026-09-12 17:50:26 +0700

    [dev] replace weather forecast endpoint with todo api welcome message

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -31,6 +31,18 @@ app.MapGet("/api/todos/{id}",(int id)=>
 });
 
 
+app.MapPost("/api/todos",(TodoPostDto dto) =>
+{
+    var nextId = todos.Count == 0 ? 1 : todos.Max(t=> t.Id+1);
+
+    var todo = new TodoGetDto(nextId,dto.Titel,false);
+    todos.Add(todo);
+    
+    return Results.Created($"/api/todos/{todo.Id}",todo);
+    
+});
+
+
 app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
